5d95b810ce9bf5688fc2fac1751cd40b1b818d8a
Riocodex <onwukachibike@gmail.com>
2023-11-14 05:55:02 +0100

    added testing functionlaities

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
-
-  //intializing state variables
+  // State variables for managing campaigns, search query, name search, and date range search
   const [campaigns, setCampaigns] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [nameSearch, setNameSearch] = useState('');
   const [dateRangeSearch, setDateRangeSearch] = useState('');
 
-  //dummy data
-  const data = [
+  // Sample campaign data
+  const initialData = [
     {"id":1,"name":"Divavu","startDate":"9/19/2017","endDate":"3/9/2018","budget":88377},
     {"id":2,"name":"Jaxspan","startDate":"11/21/2017","endDate":"2/21/2027","budget":608715},
     {"id":3,"name":"Miboo","startDate":"11/1/2017","endDate":"6/20/2017","budget":239507},
@@ -23,12 +22,11 @@ function App() {
     {"id":10,"name":"Realbridge","startDate":"3/5/2018","endDate":"10/2/2017","budget":505602}
   ];
 
-
   useEffect(() => {
     // Map the data and calculate the status (active or inactive) based on the date
     const currentDate = new Date();
 
-    const mappedCampaigns = data.map(campaign => {
+    const mappedCampaigns = initialData.map(campaign => {
       const startDate = new Date(campaign.startDate);
       const endDate = new Date(campaign.endDate);
 
@@ -44,72 +42,83 @@ function App() {
       };
     });
 
-        const filteredCampaigns = mappedCampaigns.filter(campaign =>
-          campaign.name.toLowerCase().includes(nameSearch.toLowerCase()) &&
-          isDateInRange(campaign.startDate, dateRangeSearch) &&
-          isDateInRange(campaign.endDate, dateRangeSearch)
-        );
+    // Filter campaigns based on name and date range
+    const filteredCampaigns = mappedCampaigns.filter(campaign =>
+      campaign.name.toLowerCase().includes(nameSearch.toLowerCase()) &&
+      isDateInRange(campaign.startDate, dateRangeSearch) &&
+      isDateInRange(campaign.endDate, dateRangeSearch)
+    );
 
-    
+    // Set the filtered campaigns to state
+    setCampaigns(filteredCampaigns);
+  }, [nameSearch, dateRangeSearch]);
 
-        setCampaigns(filteredCampaigns);
-    }, [nameSearch, dateRangeSearch]);
+  // Function to check if a date is within the specified range
+  const isDateInRange = (dateString, searchDateRange) => {
+    const date = new Date(dateString);
 
-    //date filter function
-    const isDateInRange = (dateString, searchDateRange) => {
-      const date = new Date(dateString);
-    
-      // Parse the searchDateRange into start and end dates
-      const [startString, endString] = searchDateRange.split('-').map(str => str.trim());
-    
-      try {
-        const startDate = startString ? new Date(startString) : null;
-        const endDate = endString ? new Date(endString) : null;
-    
-        // Check if the end date is after or equal to the start date
-        if (startDate && endDate && endDate < startDate) {
-          return false; // End date before start date, not within range
-        }
-    
-        // Check if the date is within the specified range
-        return (
-          (!startDate || date >= startDate) &&
-          (!endDate || date <= endDate)
-        );
-      } catch (error) {
-        // Handle parsing errors
-        console.error("Error parsing date:", error);
-        return false;
+    // Parse the searchDateRange into start and end dates
+    const [startString, endString] = searchDateRange.split('-').map(str => str.trim());
+
+    try {
+      const startDate = startString ? new Date(startString) : null;
+      const endDate = endString ? new Date(endString) : null;
+
+      // Check if the end date is after or equal to the start date
+      if (startDate && endDate && endDate < startDate) {
+        return false; // End date before start date, not within range
       }
-    };
 
+      // Check if the date is within the specified range
+      return (
+        (!startDate || date >= startDate) &&
+        (!endDate || date <= endDate)
+      );
+    } catch (error) {
+      // Handle parsing errors
+      console.error("Error parsing date:", error);
+      return false;
+    }
+  };
 
-        //getting input values
-        const handleNameSearchChange = event => {
-          setNameSearch(event.target.value);
-        };
-      
-        const handleDateRangeSearchChange = event => {
-          setDateRangeSearch(event.target.value);
-        };
+  // Function to add campaigns to the existing list
+  window.addCampaigns = (newCampaigns) => {
+    // Combine new campaigns with existing campaigns
+    const updatedCampaigns = [...campaigns, ...newCampaigns];
 
+    // Set the updated campaigns to state
+    setCampaigns(updatedCampaigns);
+  };
+
+  // Event handlers for input changes
+  const handleNameSearchChange = event => {
+    setNameSearch(event.target.value);
+  };
+
+  const handleDateRangeSearchChange = event => {
+    setDateRangeSearch(event.target.value);
+  };
+
+  // JSX structure for rendering the component
   return (
-    //frontend
     <div>
       <div className="search-container">
+        {/* Input for searching by name */}
         <input
-            type="text"
-            placeholder="Search by name"
-            value={nameSearch}
-            onChange={handleNameSearchChange}
-          />
-          <input
-            type="text"
-            placeholder="Search by date range (e.g., 2023-01-01 - 2023-12-31)"
-            value={dateRangeSearch}
-            onChange={handleDateRangeSearchChange}
-          />
-         </div>
+          type="text"
+          placeholder="Search by name"
+          value={nameSearch}
+          onChange={handleNameSearchChange}
+        />
+        {/* Input for searching by date range */}
+        <input
+          type="text"
+          placeholder="Search by date range (e.g., 2023-01-01 - 2023-12-31)"
+          value={dateRangeSearch}
+          onChange={handleDateRangeSearchChange}
+        />
+      </div>
+      {/* Table structure for displaying campaigns */}
       <table id="campaignTable">
         <tr className="active">
           <th>Name</th>
@@ -118,10 +127,12 @@ function App() {
           <th>End Date</th>
           <th>Budget (in USD)</th>
         </tr>
+        {/* Mapping through filtered campaigns to render table rows */}
         {campaigns.map(campaign => (
           <tr key={campaign.id} className={campaign.status}>
             <td>{campaign.name}</td>
             <td className={campaign.status}>
+              {/* Displaying status icon based on campaign status */}
               <span className="status-icon">
                 {campaign.status === 'active' ? '\u2713' : '\u2717'}
               </span> {campaign.status.charAt(0).toUpperCase() + campaign.status.slice(1)}
@@ -136,4 +147,5 @@ function App() {
   );
 }
 
+// Exporting the App component
 export default App;
